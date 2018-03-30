5ae9b9b0cac2bbfd1d04117112098a20060a07d2
Steve Brewer <steve.brewer@futurelearn.com>
2018-03-30 17:11:53 +0100

    Create home page

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="Steve" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
